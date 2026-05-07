6d3b0635d4e006e0a3c94483d4384223452b90d7
Ansh <puri.ansh05@gmail.com>
2026-05-07 22:16:26 +0530

    Update GitHub link in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto px-4">
         <a
-          href="https://github.com/ansh"
+          href="https://github.com/anshcodes22"
           target="_blank"
           rel="noopener noreferrer"
           className="text-sm font-light text-black transition-colors duration-500 ease-in-out hover:text-white"
